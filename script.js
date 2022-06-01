09840bdb879d16a72daa7d7e98860cafa0995391
Talyo Joswa <jojogiorno44@gmail.com>
2022-06-01 14:51:58 -0800

    Make a switch case in the playRound function

--- script.js
+++ script.js
@@ -3,9 +3,40 @@ function computerPlay() {
     return options[Math.floor(Math.random() * 3) + 1];
 }
 
-let computerSelection = computerPlay();
-let playerSelection = 'Rock';
+function playRound() {
+    let computerSelection = computerPlay();
+    let playerSelection = prompt("Rock, Paper or Scissors!");
 
-playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    console.log(computerSelection);
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-console.log(playerSelection);
+    let result;
+    switch(true) {
+        case playerSelection === computerSelection:
+            result = "Draw!";
+            break;
+        case playerSelection === 'Rock' && computerSelection === 'Scissors':
+            result = 'You Win! Rock beats Scissors.';
+            break;
+        case playerSelection === 'Rock' && computerSelection === 'Paper':
+            result = 'You Lose! Paper beats Rock.';
+            break;
+        case playerSelection === 'Paper' && computerSelection === 'Rock':
+            result = 'You win! Paper beats Rock.';
+            break;
+        case playerSelection === 'Paper' && computerSelection === 'Scissors':
+            result = 'You lose! Scissors beats Paper.';
+            break;
+        case playerSelection === 'Scissors' && computerSelection === 'Rock':
+            result = 'You lose! Rock beats Scissors.';
+            break;
+        case playerSelection === 'Scissors' && computerSelection === 'Paper':
+            result = 'You win! Scissors beats Paper.';
+            break;
+        default:
+            result = "Enter a valid input!";
+    }
+
+    return result;
+}
+
